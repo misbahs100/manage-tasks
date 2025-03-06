21494e5e07496e7683e3342d720f5d46d025880a
misbahs100 <misbahuddins100@gmail.com>
2025-03-06 12:41:51 +0600

    new build again

--- client/src/redux/slices/apiSlice.js
+++ client/src/redux/slices/apiSlice.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 // const API_URI = "http://localhost:8800";
 const API_URI = import.meta.env.VITE_APP_BASE_URL;
 
-const baseQuery = fetchBaseQuery({ baseUrl: "http://localhost:8800/api" });
+const baseQuery = fetchBaseQuery({ baseUrl: "/api" });
 
 export const apiSlice = createApi({
   baseQuery,
--- client/vite.config.js
+++ client/vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     proxy: {
       "/api": {
         //target: "https://task-manager-beta-silk.vercel.app/",
-        target: "http://localhost:8800",
+        target: "http://localhost:8800/api",
 
         changeOrigin: true,
       },
