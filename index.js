d406c7b83c9f4b30f3eacaaca2c87813bd3de728
misbahs100 <misbahuddins100@gmail.com>
2025-03-06 12:33:59 +0600

    env changed

--- index.js
+++ index.js
@@ -20,13 +20,13 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 
-// app.use(
-//   cors({
-//     origin: "*",
-//     methods: ["GET", "POST", "PUT", "DELETE"],
-//     credentials: true,
-//   })
-// );
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
